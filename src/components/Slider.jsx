51c2f99db3dfd13984092e50911ecfe66037c3f5
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-13 23:12:44 -0500

    add slider function, and props to wrapper

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,7 +34,7 @@ const Arrow = styled.div `
 const Wrapper = styled.div `
   height: 100%;
   display: flex;
-  transform: translateX(0vw);
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div `
@@ -89,7 +89,11 @@ const Slider = () => {
 const  [slideIndex, setSlideIndex] = useState('');
     
 const handleClick = (direction) => {
-
+   if(direction === "left") {
+       setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+   } else {
+       setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+   }
 }
 
     return (
@@ -97,7 +101,7 @@ const handleClick = (direction) => {
             <Arrow direction="left" onClick={() => handleClick("left")}>
                 <ArrowLeftOutlined />
             </Arrow>
-            <Wrapper>
+            <Wrapper slideIndex={slideIndex}>
             {sliderItems.map((item) => (
               <Slide bg="beige">
                <ImgContainer>
