c40e549ea6a9a51fa2352e7a40621527cd5f3559
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-14 21:58:34 -0500

    add slider component with arrows icons and styling

---
+++ src/components/Slider.jsx
@@ -0,0 +1,30 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div `
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`
+
+const Arrow = styled.div `
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+`
+
+const Slider = () => {
+    return (
+        <Container>
+            <Arrow>
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Arrow>
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
+}
+
+export default Slider;
