389cbc790805cf74d95f6679d24f59578c817c12
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-16 18:11:32 -0500

    add register and login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div `
+
+`;
+
+const Login = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div `
+
+`;
+
+const Register = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Register;
