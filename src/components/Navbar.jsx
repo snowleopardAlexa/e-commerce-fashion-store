41e7e4387653c8fd5ae484985ea81b3efcd033dd
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-15 21:01:27 -0500

    write first styled comp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div `
+   height: 60px;
+   background-color: black;
+`
 
 const Navbar = () => {
     return (
-        <div>
-        navbar
-        </div>
+        <Container>
+           navbar
+        </Container>
     )
 }
 
