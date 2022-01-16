bcd1605d795941253a8c4ceaff03889ecc690f64
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-15 21:06:12 -0500

    add flexbox and 3 divs: left, center, right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,27 @@ import styled from 'styled-components';
 
 const Container = styled.div `
    height: 60px;
-   background-color: black;
 `
+
+const Wrapper = styled.div `
+   padding: 10px 20px;
+   display: flex;
+`
+
+const Left = styled.div ``
+
+const Center = styled.div ``
+
+const Right = styled.div ``
 
 const Navbar = () => {
     return (
         <Container>
-           navbar
+           <Wrapper>
+               <Left>Left</Left>
+               <Center>center</Center>
+               <Right>right</Right>
+           </Wrapper>
         </Container>
     )
 }
