87ae12361afb2e750cb5630663af6a0622aa30c7
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-16 17:39:40 -0500

    add components

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,12 +8,59 @@ import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
 
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  ${mobile({ padding: "10px", flexDirection:"column" })}
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+  ${mobile({ height: "40vh" })}
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
 const Product = () => {
     return (
      <Container>
       <Navbar />
       <Announcement />
-     
+      <Wrapper>
+        <ImgContainer>
+          <Image src="" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Satin Golden Dress
+          </Desc>
+          <Price>$ 200</Price>
+          </InfoContainer>
+          </Wrapper>
       <Newsletter />
       <Footer />
     </Container>
