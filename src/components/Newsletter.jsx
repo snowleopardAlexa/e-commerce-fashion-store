32ca9134162667441cb7e65706c2c5095f8aa33c
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-16 14:28:49 -0500

    style newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,30 +4,45 @@ import styled from 'styled-components';
 
 const Container = styled.div `
   height: 60vh;
-  background-color: yellow;
+  background-color: #880808;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  color: white;
 `;
 
 const Title = styled.h1 `
-
+  font-size: 4rem;
+  margin-bottom: 20px;
 `;
 
 const Desc = styled.div `
-
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+  font-weight: 300;
 `;
 
 const InputContainer = styled.div `
-
+  width: 50%;
+  height: 40px;
+  background-color: pink;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
 `;
 
 const Input = styled.input `
-
+  border: transparent;
+  flex: 8;
+  padding-left: 20px;
 `;
 
 const Button = styled.button `
-
+  flex: 1;
+  border: transparent;
+  color: white;
+  background-color: black;
 `;
 
 const Newsletter = () => {
